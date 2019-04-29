1260b6d8ccb294add844f534cb2d7106baaa7777
carlosandiaz <carlosandiaz@gmail.com>
2019-04-29 16:51:40 +0200

    Search ok

--- routes/authors.js
+++ routes/authors.js
@@ -3,9 +3,22 @@ const router = express.Router();
 const Author = require('../models/author')
 
 /* All Authors Route */
-router.get('/', (req, res, next) => {
-  res.render('authors/index');
-});
+router.get('/', async (req, res, next) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '' ){
+
+      searchOptions.name = new RegExp(req.query.name, 'i')
+      
+  }
+  try{ 
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index' , { 
+    authors: authors, searchOptions: req.query
+    }) 
+  } catch{
+    res.redirect('/')
+  }
+})
 
 /* New Author Route */
 router.get('/new', (req, res, next) => {
@@ -13,22 +26,27 @@ router.get('/new', (req, res, next) => {
   });
 
 /* Create Author Route */
-router.post('/' , (req, res, next) => {
+router.post('/' , async (req, res, next) => {
     const author = new Author({
       name:req.body.name
     })
-    author.save((err, newAuthor) => {
-      if (err){
-        res.render('authors/new', {
-          author: author,
-          errorMessage: 'Error creating Author'
-        })
-      } else {
-        
-         //res.redirect(`authors/{{newAuthor.id}}`)
-         res.redirect(`authors`)
-      }
-    })
-  });
+    try{ 
+      const newAuthor = await author.save()
+      res.redirect(`authors`)
+     
+    } catch{
+      res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error creating Author'
+      })
+    }
+  })
+
+
+
+
+
+
+
 
 module.exports = router;
